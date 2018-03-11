6f91a30f068c7798465602c5c6518462510c72f0
prakharrr <pxr7185@g.rit.edu>
2018-03-11 15:19:58 -0400

    Initial Commit

---
+++ HostApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HostApp
+//
+//  Created by Prakhar Rawat on 3/11/18.
+//  Copyright © 2018 Prakhar Rawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HostApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HostApp
+//
+//  Created by Prakhar Rawat on 3/11/18.
+//  Copyright © 2018 Prakhar Rawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
